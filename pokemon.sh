ecc719f831aa4b71efd766d5e88eac99f45b0d77
Bart Willems <ps3bartwillems@gmail.com>
2016-07-22 23:27:41 +0200

    Fixed formatting
    Signed-off-by: Bart Willems <ps3bartwillems@gmail.com>

--- pokemon.sh
+++ pokemon.sh
@@ -7,9 +7,9 @@ key="$1"
 case $key in
     -j|--jar)
     JAR="$2"
-		# The game path should be an absolute path
-		GAME_PATH=$(dirname $JAR)
-		CONF_PATH=$GAME_PATH/config.properties
+    # The game path should be an absolute path
+    GAME_PATH=$(dirname $JAR)
+    CONF_PATH=$GAME_PATH/config.properties
     shift 
     ;;
     -lo|--long)
@@ -25,11 +25,11 @@ case $key in
     shift
     ;;
     -p|--password)
-		PASSW=true
+    PASSW=true
     shift
     ;;
     *)
-            # unknown option
+    # unknown option
     ;;
 esac
 shift # past argument or value
@@ -37,22 +37,22 @@ done
 
 #SET PARAM
 if [[ $PASSW ]]; then
-		BASE_PASS=$(read -s -p "Enter your password")
-		BASE_PASS=$(echo "$BASE_PASS" | base64)
-		sed -i "/^base64_password=/c\base64_password=$BASE_PASS" $CONF_PATH
-		sed -i "/^password=/c\password=" $CONF_PATH
+    BASE_PASS=$(read -s -p "Enter your password")
+    BASE_PASS=$(echo "$BASE_PASS" | base64)
+    sed -i "/^base64_password=/c\base64_password=$BASE_PASS" $CONF_PATH
+    sed -i "/^password=/c\password=" $CONF_PATH
 fi
 
 if [[ "$USERNAME" ]]; then
-		sed -i "/^username=/c\username=$USERNAME" $CONF_PATH
+    sed -i "/^username=/c\username=$USERNAME" $CONF_PATH
 fi
 
 if [[ "$LONG" ]]; then
-		sed -i "/^longitude=/c\longitude=$LONG" $CONF_PATH
+    sed -i "/^longitude=/c\longitude=$LONG" $CONF_PATH
 fi
 
 if [[ "$LAT" ]]; then
-		sed -i "/^latitude=/c\latitude=$LAT" $CONF_PATH
+    sed -i "/^latitude=/c\latitude=$LAT" $CONF_PATH
 fi
 
 #RUN GAME
@@ -61,8 +61,8 @@ OUTPOUT=$(tail -n 10 nohup.out)
 AUTH_CODE_STR=$($OUTPUT | grep 'https://www.google.com/device')
 
 if [[ $AUTH_CODE_STR ]];then
-		$AUTH_CODE_STR=$(echo $AUT_CODE_STR | rev | cut -d: -f1 | rev)
-		echo "Please enter the code: $AUTH_CODE_STR in https://www.google.com/device"
+    $AUTH_CODE_STR=$(echo $AUT_CODE_STR | rev | cut -d: -f1 | rev)
+    echo "Please enter the code: $AUTH_CODE_STR in https://www.google.com/device"
 fi
 
 tail -f nohup.out | grep Caught
