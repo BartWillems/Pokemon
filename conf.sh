ef2494f7c9f238fe5c34567ec8f033f8ffd16a91
Bart Willems <ps3bartwillems@gmail.com>
2016-07-25 21:26:53 +0200

    Added pokevision coördinate extraction script 3000
    Signed-off-by: Bart Willems <ps3bartwillems@gmail.com>

---
+++ conf.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# This script accepts a link from the pokévision.com website, 
+# extracts the coördinates, places them in the conf file
+# and removes the token(since it's borked since version 0.4)
+
+URL=$1
+
+LINK=${URL%@*}
+COORDINATE=${URL##*@}
+
+LAT=${COORDINATE%,*}
+LONG=${COORDINATE##*,}
+
+sed -i "/^token=/c\token=" config.properties
+sed -i "/^latitude=/c\latitude=$LAT" config.properties
+sed -i "/^longitude=/c\longitude=$LONG" config.properties
